4f7b5e7d73fa49b5a70bff2d09ee39a1113a6bb2
Dominic White <domw30@gmail.com>
2019-03-28 14:29:26 +0000

    refactor code

---
+++ histogram.rb
@@ -0,0 +1,18 @@
+puts "Enter words"
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each do |element|
+  frequencies[element] += 1
+end
+
+frequencies = frequencies.sort_by do |key, count|
+  count
+end
+
+frequencies.reverse!
+
+frequencies.each do |key, value|
+  puts key + " " + value.to_s
+end
